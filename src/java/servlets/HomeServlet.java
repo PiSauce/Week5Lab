07f77e0cef7504d95d29821af61f03369ba3a6b4
Aaron Ribano <aaronribano@yahoo.ca>
2023-02-24 19:22:04 -0700

    login page works :D

--- src/java/servlets/HomeServlet.java
+++ src/java/servlets/HomeServlet.java
@@ -1,20 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(name = "HomeServlet", urlPatterns = {"/home"})
+/**
+ *
+ * @author Xhunt
+ */
 public class HomeServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
+        getServletContext().getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
     }
 
     @Override
@@ -22,5 +29,5 @@ public class HomeServlet extends HttpServlet {
             throws ServletException, IOException {
         
     }
-    
+
 }
